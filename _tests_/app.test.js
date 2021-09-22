1de5a5092459f9c1bddf072fd7f715eb7064c63e
TANNER MECK <steve.meck246@gmail.com>
2021-09-22 16:15:17 -0700

    first test passing

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -9,12 +9,15 @@ jest.mock('twilio', () => () => ({
     create: jest.fn()
   }
 }));
-
 describe('03_separation-of-concerns-demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-
+  beforeEach(() => {
+    return request(app)
+      .post('/api/v1/orders')
+      .send({ id: '1', quantity: 2 });
+  });
   it('creates a new order in our database and sends a text message', () => {
     return request(app)
       .post('/api/v1/orders')
@@ -22,9 +25,30 @@ describe('03_separation-of-concerns-demo routes', () => {
       .then(res => {
         // expect(createMessage).toHaveBeenCalledTimes(1);
         expect(res.body).toEqual({
-          id: '1',
+          id: '2',
           quantity: 10
         });
       });
+  });
+  it('returns all orders using GET from our database and sends a text message', async() => {
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+    return request(app)
+      .get('/api/v1/orders')
+      .then(res => {
+        // expect(createMessage).toHaveBeenCalledTimes(1);
+        expect(res.body).toEqual([{
+          id: '1',
+          quantity: 2
+        }, {
+          id: '2',
+          quantity: 10
+        }]);
+      });
+  });
+
+  it('returns the order using GET /:id with a matching id and quantity and sends a text message', async() => {
+
   });
 });
