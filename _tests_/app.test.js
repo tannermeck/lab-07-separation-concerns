29803058b650d670f42c1198845532c9e48bf1ac
TANNER MECK <steve.meck246@gmail.com>
2021-09-22 16:19:47 -0700

    second test get:id setup

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -16,7 +16,7 @@ describe('03_separation-of-concerns-demo routes', () => {
   beforeEach(() => {
     return request(app)
       .post('/api/v1/orders')
-      .send({ id: '1', quantity: 2 });
+      .send({ quantity: 2 });
   });
   it('creates a new order in our database and sends a text message', () => {
     return request(app)
@@ -49,6 +49,13 @@ describe('03_separation-of-concerns-demo routes', () => {
   });
 
   it('returns the order using GET /:id with a matching id and quantity and sends a text message', async() => {
-
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 3 });
+    return request(app)
+      .get('/api/v1/orders/2')
+      .then(res => {
+        expect(res.body).toEqual({ id: '2', quantity: 3 });
+      });
   });
 });
